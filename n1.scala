bf6918132c08b75e22e348694d4b7ecce53443e0
815421104028au <146175382+815421104028au@users.noreply.github.com>
2025-05-02 15:05:02 +0530

    Create n1.scala
    Collection is a container to carry method and class and interface

---
+++ n1.scala
@@ -0,0 +1,28 @@
+import scala.collection.mutable.ArrayBuffer
+class Numbers {
+val num= ArrayBuffer[Int]() // cteate ArrayBuffer
+} // class  is need for sime specific  behaviour like encapsulation 
+object n1{
+def main(args: Array[String]):Unit = {
+val n =  new Numbers ()
+n.num += 4;
+n.num += 5;
+n.num += 6;  //add elements to buffer
+n.num += 7;
+println(n.num)
+n.num.append(8, 9, 10) //suffix multiple 
+println (n.num)
+n.num.prepend(1,2,3) //prefix multiple 
+println(n.num)
+//n.num -= 9
+n.num --=Seq(9, 10) //remove all with specific 
+println(n.num)
+n.num.remove(2) //remove some particular
+println(n.num)
+n.num.insert (2,3) //insert specific position to add element
+println(n.num)
+n.num.remove(0, 3) //remove multiple with specific 
+
+println (n.num)
+
+}
